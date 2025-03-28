16d379f8abb1584b9336e16b153a6b33b8210500
znikolaou <ZachariasMNic@gmail.com>
2025-03-28 09:53:21 +0200

    Minor reformatting.

--- src/global.f90
+++ src/global.f90
@@ -1,17 +1,24 @@
       MODULE GLOBAL
       
-      INTEGER, PARAMETER :: NREMX=6000,NSPMX=300,NSMX=500, &
-                            NLINEMX=10000,NSFLMX=1000
-      CHARACTER(LEN=*), PARAMETER :: INDIR='./input/', &
-                                     OUTDIR='./output/', &
-                                     RATE_DIR='rates/', &
-                                     CONTROL_FL='control.txt', &
-                                 CHEMRED_FL='reducedChemistry.txt', &
-                                 RATE_FL='speciesMeanRatesMatrix.dat', &
-                                 REACTION_RATE_FL='reactionRates', &
-                                     EQ_SEP='=>'
-      DOUBLE PRECISION, PARAMETER :: ZERO=0.0E0,ONE=1.0E0
-                                     
+      CHARACTER(LEN=*), PARAMETER :: &
+                        INDIR='./input/',   &
+                        OUTDIR='./output/', &
+                        RATE_DIR='rates/',  &
+                        CONTROL_FL='control.txt',      &
+                        CHEMRED_FL='reducedChemistry.txt', &
+                        RATE_FL='speciesMeanRatesMatrix.dat', &
+                        REACTION_RATE_FL='reactionRates', &
+                        EQ_SEP='=>'
+
+      INTEGER, PARAMETER :: NREMX=6000,    &
+                            NSPMX=300,     &
+                            NSMX=500,      &
+                            NLINEMX=10000, &
+                            NSFLMX=1000 
+
+      DOUBLE PRECISION, PARAMETER :: ZERO=0.0E0, &
+                                     ONE=1.0E0
+      !-----------------------------------------------------------------                                     
       INTEGER :: NELEM,NSPEC,NREAC,NSPEC_BOLSIG,NBOLS_SET,NREAC_DOLLAR
       CHARACTER(LEN=NSMX) :: ELEM(NSPMX),SPEC(NSPMX),REAC(NREMX), &
                              REACF(NREMX), &
@@ -26,5 +33,6 @@
       INTEGER :: RSPEC(NREMX,NSPMX)
       DOUBLE PRECISION :: NUR(NREMX,NSPMX),NUP(NREMX,NSPMX), &
                           DELTANU(NREMX,NSPMX),SPEC_CHARGE(NSPMX)
+      !-----------------------------------------------------------------            
 
       END MODULE GLOBAL
