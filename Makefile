53ce00ac8fb8e6bc4d2432caffee4704c573d130
znikolaou <ZachariasMNic@gmail.com>
2025-05-21 12:24:20 +0300

    Reformatted Makefile and setevn.sh

--- Makefile
+++ Makefile
@@ -1,21 +1,26 @@
 
-SRC=$(SRC_DIR)/global.f90 $(SRC_DIR)/io.f90 $(SRC_DIR)/chem_parse.f90 \
-    $(SRC_DIR)/graph_search.f90 $(SRC_DIR)/main.f90  \
-    $(SRC_DIR)/drg.f90 $(SRC_DIR)/output.f90 $(SRC_DIR)/charUtil.f90 $(SRC_DIR)/timing.f90 \
-    $(SRC_DIR)/sort.f90 $(SRC_DIR)/set.f90
+SRC_IN=global.f90 io.f90 chem_parse.f90 \
+       graph_search.f90 main.f90  \
+       drg.f90 output.f90 charUtil.f90 timing.f90 \
+       sort.f90 set.f90
 
-OBJS=$(patsubst $(SRC_DIR)/%.o,$(BUILD_DIR)/%.o,$(SRC:%.f90=%.o)) 
+SRC=$(addprefix $(SRC_DIR),$(notdir $(SRC_IN)))
+
+OBJS=$(patsubst $(SRC_DIR)%.o,$(BUILD_DIR)%.o,$(SRC:%.f90=%.o)) 
 
 redChem:$(OBJS) 
 	$(FC) $(FOPT) $(OBJS) -o $(BIN_DIR)/$@
 
-$(BUILD_DIR)/%.o: $(SRC_DIR)/%.f90
+$(BUILD_DIR)%.o: $(SRC_DIR)%.f90
 	$(FC) $(FOPT) -c $< -o $@
 
 clean:
 	@ rm -f $(BIN_DIR)/* *.o *.mod $(BUILD_DIR)/*.o 
 
-print:
+printObjs:
 	@ echo $(OBJS)
+
+printSrc:
+	@ echo $(SRC)
 
 
--- setenv.sh
+++ setenv.sh
@@ -1,26 +1,40 @@
 #!/bin/bash
+# Author: Z. Nikolaou (2025)
+# Shell script for setting env. vars.
+#
+
+# Set below Fortran compiler and compiler options:
+FC='gfortran'
+FOPT='-O3 -mcmodel=large -fbounds-check'
+
+#----------------------------------------------------------------------#
+#--------------------No editing below this line -----------------------#
 
 WRK_DIR=$(pwd)
 SRC_DIR=$WRK_DIR/src/
 BUILD_DIR=$WRK_DIR/build/
 BIN_DIR=$WRK_DIR/bin/
 
-FC='gfortran'
-FOPT='-O3 -mcmodel=large -fbounds-check'
-
 echo " "
-echo "Setting environment vars"
+echo " **********plasRedChem_v0.1**********"
+echo " Plasma-chemistry reduction library. "
+echo " Author: Z. Nikolaou (2025)"
+echo " ************************************"
 echo " "
-echo "Work dir:"
-echo $WRK_DIR
-echo "Fortran compiler:"
-echo $FC
-echo "Compiler options:"
-echo $FOPT
-echo "Build dir:"
-echo $BUILD_DIR
-echo "Bin dir:"
-echo $BIN_DIR
+echo " Setting environment vars ..."
+echo " Work dir:"
+echo " "$WRK_DIR
+echo " Build dir:"
+echo " "$BUILD_DIR
+echo " Bin dir:"
+echo " "$BIN_DIR
+echo " Fortran compiler:"
+echo " "$FC
+echo " Compiler options:"
+echo " "$FOPT
+echo ""
+echo " Environment variables set! Please make clean and make redChem to compile."
+echo " "
 
 export PATH=$PATH:$BIN_DIR
 export WRK_DIR
